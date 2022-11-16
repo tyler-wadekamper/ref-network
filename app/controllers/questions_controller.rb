e1dd5503ba2de51fca5fd848338214c46ad73073
tyler-wadekamper <wadekampert@spu.edu>
2022-11-16 15:21:21 -0800

    Add edit functionality for questions.

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -19,9 +19,23 @@ class QuestionsController < ApplicationController
     end
   end
 
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+
+    if @question.update(question_params)
+      redirect_to questions_url
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def question_params
-    params.require(:question).permit(:body, :commit)
+    params.require(:question).permit(:body, :commit, :id)
   end
 end
