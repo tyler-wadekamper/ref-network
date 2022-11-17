91902067ff039a47cdbee9710914a787aa54db20
tyler-wadekamper <wadekampert@spu.edu>
2022-11-17 12:01:47 -0800

    Add separate attributes for user first and last name. Add capitalization of each name.

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :questions, foreign_key: :author_id, class_name: 'Question'
+
+  before_save do
+    self.first_name = first_name.capitalize
+    self.last_name = last_name.capitalize
+  end
 end
