c0d7e3bb40f8b19a7b2e358b20e19ea655726763
tyler-wadekamper <wadekampert@spu.edu>
2022-12-22 10:22:36 -0800

    Add feature test for unsuccessful question creation.

--- test/system/feature_test.rb
+++ test/system/feature_test.rb
@@ -21,6 +21,25 @@ class FeatureTest < ApplicationSystemTestCase
     assert_selector "button.answer-button", text: "Hide Answer"
   end
 
+  def fill_in_question(valid: true)
+    fake_body = Faker::Lorem.paragraph
+
+    body_content = fake_body if valid
+    body_content = "" unless valid
+
+    fill_in "question[body]", with: body_content
+
+    select VALID_TEAMS.sample, from: "question[answer_attributes][team]"
+    select VALID_DOWNS.sample, from: "question[answer_attributes][down]"
+    select VALID_DISTANCE.sample, from: "question[answer_attributes][distance]"
+    select VALID_TEAMS.sample, from: "question[answer_attributes][yardline_team]"
+    select VALID_YARDLINE_NUM.sample, from: "question[answer_attributes][yardline_num]"
+    select VALID_CLOCK_STATUS.sample, from: "question[answer_attributes][clock_status]"
+
+    fill_in "question[answer_attributes][explanation]", with: Faker::Lorem.paragraph
+    body_content
+  end
+
   class QuestionListTest < FeatureTest
     setup do
       create_questions(50)
@@ -68,44 +87,47 @@ class FeatureTest < ApplicationSystemTestCase
   class AddQuestionTest < FeatureTest
     setup do
       create_questions(20)
+      @user2 = create_random_user
+      sign_in @user2
+      visit questions_url
+      click_on "New Question"
     end
 
     class SuccessfulAddTest < AddQuestionTest
       test "creates a new question" do
-        user2 = create_random_user
-        sign_in user2
-        visit questions_url
-
-        click_on "New Question"
-
-        fake_body = Faker::Lorem.paragraph
-
-        fill_in "question[body]", with: fake_body
-
-        select VALID_TEAMS.sample, from: "question[answer_attributes][team]"
-        select VALID_DOWNS.sample, from: "question[answer_attributes][down]"
-        select VALID_DISTANCE.sample, from: "question[answer_attributes][distance]"
-        select VALID_TEAMS.sample, from: "question[answer_attributes][yardline_team]"
-        select VALID_YARDLINE_NUM.sample, from: "question[answer_attributes][yardline_num]"
-        select VALID_CLOCK_STATUS.sample, from: "question[answer_attributes][clock_status]"
-
-        fill_in "question[answer_attributes][explanation]", with: Faker::Lorem.paragraph
+        body_content = fill_in_question(valid: true)
 
         click_on "Create Question"
+
+        sleep(0.5)
 
         question = Question.order("created_at").last
         answer = question.answer
 
         within "turbo-frame#question_#{question.id}" do
-          assert_selector "div.author-name", text: "#{@user.first_name} #{@user.last_name}"
+          assert_selector "div.author-name", { count: 1, text: "#{@user2.first_name} #{@user2.last_name}" }
+          assert_selector "div.body", { count: 1, text: body_content }
           assert_answer_content(answer)
         end
-
-        assert_selector "div.body", { count: 1, text: fake_body }
       end
     end
 
     class UnsuccessfulAddTest < AddQuestionTest
+      test "does not create a question" do
+        fill_in_question(valid: false)
+
+        click_on "Create Question"
+
+        within "turbo-frame#new_question" do
+          assert_selector "div.errors" do
+            assert_selector ".text-danger", text: "1 error prohibited this question from being saved:"
+          end
+        end
+
+        assert_selector "div.main-text"
+        assert_selector "div.answer"
+        assert_selector "div.submit-button"
+      end
     end
   end
 end
