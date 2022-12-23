3a773f11a2156e92a0a4a2383f3b8c0f8dede8b9
tyler-wadekamper <wadekampert@spu.edu>
2022-12-22 18:20:58 -0800

    Add user sign in tests to user_feature_test.rb.

--- test/system/user_feature_test.rb
+++ test/system/user_feature_test.rb
@@ -19,14 +19,13 @@ class UserFeatureTest < ApplicationSystemTestCase
       fill_in "Email", with: user.email
       fill_in "Password", with: "password123"
       fill_in "Password confirmation", with: "password123"
+      click_on "commit"
       user
     end
 
     class SuccessfulRegistrationTest < RegistrationTest
       test "creates a new user" do
         user = fill_in_new_user_fields(valid: true)
-
-        click_on "commit"
 
         assert_selector "div.alert-success", text: "Welcome! You have signed up successfully."
         assert_selector "li", text: "Logged in as #{user.first_name} #{user.last_name}"
@@ -37,8 +36,6 @@ class UserFeatureTest < ApplicationSystemTestCase
       test "does not create a new user" do
         fill_in_new_user_fields(valid: false)
 
-        click_on "commit"
-        
         within "div#error_explanation" do
           assert_selector "*", text: "1 error prohibited this user from being saved:"
           assert_selector "li", text: "First name can't be blank"
@@ -47,14 +44,45 @@ class UserFeatureTest < ApplicationSystemTestCase
     end
   end
   
-  # class SignInTest < UserFeatureTest
-  #   class SuccessfulSignInTest < SignInTest
-  #   end
+  class SignInTest < UserFeatureTest
+    setup do
+      @user = create_default_user
+      visit new_user_session_url
+    end
 
-  #   class UnsuccessfulSignInTest < SignInTest
-  #   end
-  # end
+    def fill_in_sign_in_fields(valid: true)
+      fill_in "Email", with: @user.email
+      fill_in "Password", with: "UserOnePass" if valid
+      fill_in "Password", with: "InvalidPassword" unless valid
+      click_on "commit"
+    end
+    class SuccessfulSignInTest < SignInTest
+      test "signs in successfully" do
+        fill_in_sign_in_fields(valid: true)
+
+        assert_selector "div.alert-success", text: "Signed in successfully."
+        assert_selector "li", text: "Logged in as #{@user.first_name} #{@user.last_name}"
+      end
+    end
+
+    class UnsuccessfulSignInTest < SignInTest
+      test "does not sign in" do
+        fill_in_sign_in_fields(valid: false)
+
+        assert_selector "div.alert-danger", text: "Invalid Email or password."
+        assert_selector "input#user_email"
+        assert_selector "input#user_password"
+      end
+    end
+  end
 
   # class PasswordResetTest < UserFeatureTest
+  #   setup do
+  #     visit new_user_password_url
+  #   end
+
+  #   test "sends the password reset email" do
+
+  #   end
   # end
 end
