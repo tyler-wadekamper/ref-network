0a01eebedf118f98dc6a7510d12720e375fede1f
tyler-wadekamper <wadekampert@spu.edu>
2023-01-06 14:17:39 -0800

    Edit seed script to properly generate each reference. Fix errors in the reference seed file.

--- db/seeds/reference_seeds.rb
+++ db/seeds/reference_seeds.rb
@@ -2,16 +2,14 @@
 
 require 'csv'
 
-def assign_if_present(string, row, previous_row)
-  return row.field(string) unless previous_row
-
-  return previous_row.field(string) if field_empty?(string, row)
-
-  row.field(string)
-end
-
 def field_empty?(string, row)
   row.field(string).to_s.strip.empty?
+end
+
+def no_subarticle?(row, previous_row)
+  return true if previous_row.nil?
+
+  field_empty?('subarticle', row)
 end
 
 current_path = File.dirname(__FILE__)
@@ -20,16 +18,22 @@ file_path = File.join(current_path, 'seed_references.csv')
 previous_row = nil
 
 CSV.foreach(file_path, headers: true) do |row|
-  rule = assign_if_present('rule', row, previous_row)
-  section = assign_if_present('section', row, previous_row)
-  article = assign_if_present('article', row, previous_row)
-  subarticle = row.field('subarticle')
+  if no_subarticle?(row, previous_row)
+    rule = row.field('rule')
+    section = row.field('section')
+    article = row.field('article')
+  else
+    rule = previous_row.field('rule')
+    section = previous_row.field('section')
+    article = previous_row.field('article')
+  end
 
+  subarticle = row.field('subarticle')
   subarticle = subarticle.downcase if subarticle
 
   name = row.field('name')
 
   Reference.create!(rule:, section:, article:, subarticle:, name:)
 
-  previous_row = row unless field_empty?('rule', row)
+  previous_row = row if no_subarticle?(row, previous_row)
 end
