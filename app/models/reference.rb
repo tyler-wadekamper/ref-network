f38c5d39183051eddd46ee9f6b79f963044d71de
tyler-wadekamper <wadekampert@spu.edu>
2023-01-12 12:31:13 -0800

    Add reference associations for parent and children. Add associated tests. Add text and length methods for reference.

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -3,4 +3,23 @@ class Reference < ApplicationRecord
 
   has_many :question_references
   has_many :questions, through: :question_references
+
+  belongs_to :parent, class_name: 'Reference', optional: true
+  has_many :children, class_name: 'Reference', foreign_key: 'parent_id'
+
+  def text
+    total_text = "#{rule}"
+    total_text += "-#{section}" if section
+    total_text += "-#{article}" if article
+    total_text += ".#{subarticle}" if subarticle
+    total_text
+  end
+
+  def length
+    return 1 if section.nil?
+    return 2 if article.nil?
+    return 3 if subarticle.nil?
+
+    return 4
+  end
 end
--- test/models/reference_test.rb
+++ test/models/reference_test.rb
@@ -20,4 +20,28 @@ class ReferenceTest < ActiveSupport::TestCase
     reference = Reference.new
     assert_not reference.save
   end
+
+  test "#parent returns parent reference" do
+    child_references = create_list(:random_reference, 3)
+    parent_reference = create(:random_reference)
+
+    child_references.each do |child|
+      parent_reference.children << child
+    end
+
+    child_references.each do |child|
+      assert_equal child.parent, parent_reference
+    end
+  end
+
+  test "#children returns list of child references" do
+    child_references = create_list(:random_reference, 3)
+    parent_reference = create(:random_reference)
+
+    child_references.each do |child|
+      parent_reference.children << child
+    end
+
+    assert_equal parent_reference.children, child_references
+  end
 end
