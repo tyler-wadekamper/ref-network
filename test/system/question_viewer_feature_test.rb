4ad2882ef113abdbee817b270946535b3c832c97
tyler-wadekamper <wadekampert@spu.edu>
2023-02-17 14:09:55 -0800

    Restore viewer toggle functionality and question_viewer controller tests.

--- test/system/question_viewer_feature_test.rb
+++ test/system/question_viewer_feature_test.rb
@@ -1,19 +1,14 @@
 class QuestionFeatureTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
-
   setup do
     random_questions(20)
   end
 
   test "shows questions as viewed after clicking on the answer" do
-  end
 
-  test "remains viewed after clicking the answer again" do
   end
 
   test "remains viewed after logging out and back in" do
-  end
 
-  test "shows none viewed for a new user" do
   end
 end
