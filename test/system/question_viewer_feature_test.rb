bacd8ff4ce4b888e24c6fd609c21857cc4c4ce6e
tyler-wadekamper <wadekampert@spu.edu>
2023-02-17 16:15:18 -0800

    Ass system tests to verify question_viewer.

--- test/system/question_viewer_feature_test.rb
+++ test/system/question_viewer_feature_test.rb
@@ -1,14 +1,53 @@
-class QuestionFeatureTest < ApplicationSystemTestCase
+require "application_system_test_case"
+
+class QuestionViewerFeatureTest < ApplicationSystemTestCase
   include Devise::Test::IntegrationHelpers
+
   setup do
-    random_questions(20)
+    ActionController::Base.allow_forgery_protection = true
+    create_sample_references('6')
+    create_questions(15)
   end
 
-  test "shows questions as viewed after clicking on the answer" do
-
+  teardown do
+    ActionController::Base.allow_forgery_protection = false
   end
 
-  test "remains viewed after logging out and back in" do
+  test "shows question as viewed after clicking on the answer" do
+    @user = create_random_user
+    sign_in @user
 
+    visit questions_url
+
+    question_id = Question.last.id
+    assert_selector "#question_#{question_id}_viewer", text: /.*New question\z/
+
+    within "#question_#{question_id}" do
+      click_on "Show Answer"
+    end
+
+    assert_selector "#question_#{question_id}_viewer", text: /.*Viewed question\z/
+  end
+
+  test "question remains viewed in new session" do
+    @user = create_random_user
+    sign_in @user
+
+    visit questions_url
+
+    question_id = Question.last.id
+    assert_selector "#question_#{question_id}_viewer", text: /.*New question\z/
+
+    within "#question_#{question_id}" do
+      click_on "Show Answer"
+      sleep(0.5)
+    end
+    assert_selector "#question_#{question_id}_viewer", text: /.*Viewed question\z/
+
+    click_on "Log out"
+    sleep(0.5)
+    sign_in @user
+    visit questions_url
+    assert_selector "#question_#{question_id}_viewer", text: /.*Viewed question\z/
   end
 end
