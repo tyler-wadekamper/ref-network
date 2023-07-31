0e1d38bed60ec8fda314fcf6ce13d16a6df683c1
tyler-wadekamper <wadekampert@spu.edu>
2023-07-30 21:02:42 -0700

    Restrict voting to one per question per user.

--- app/controllers/downvotes_controller.rb
+++ app/controllers/downvotes_controller.rb
@@ -2,20 +2,26 @@ class DownvotesController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    downvote = current_user.downvotes.new(question_id: params[:question_id])
-    if downvote.save
-      render json: { success: true, net_votes: downvote.question.net_votes }
-    else
-      render json: { success: false, error: downvote.errors.full_messages.join(", ") }, status: :unprocessable_entity
-    end
-  end
-
-  def destroy
     downvote = current_user.downvotes.find_by(question_id: params[:question_id])
-    if downvote&.destroy
-      render json: { success: true, net_votes: downvote.question.net_votes }
+    upvote = current_user.upvotes.find_by(question_id: params[:question_id])
+
+    if downvote
+      render json: { success: false, error: "Already downvoted", net_votes: downvote.question.net_votes }
+    elsif upvote
+      upvote.destroy
+      new_downvote = current_user.downvotes.new(question_id: params[:question_id])
+      if new_downvote.save
+        render json: { success: true, net_votes: new_downvote.question.net_votes }
+      else
+        render json: { success: false, error: new_downvote.errors.full_messages.join(", "), status: :unprocessable_entity }
+      end
     else
-      render json: { success: false, error: "Couldn't find downvote" }, status: :unprocessable_entity
+      new_downvote = current_user.downvotes.new(question_id: params[:question_id])
+      if new_downvote.save
+        render json: { success: true, net_votes: new_downvote.question.net_votes }
+      else
+        render json: { success: false, error: new_downvote.errors.full_messages.join(", "), status: :unprocessable_entity }
+      end
     end
   end
 end
