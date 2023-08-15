e63f224d5bf08c833f790ec58c21c2f89b4880cc
tyler-wadekamper <wadekampert@spu.edu>
2023-08-14 19:06:43 -0700

    Modify weekly question email url, uncomment weekly email job.

--- app/jobs/weekly_question_email_job.rb
+++ app/jobs/weekly_question_email_job.rb
@@ -5,14 +5,14 @@ class WeeklyQuestionEmailJob < ApplicationJob
     questions = Question.top_upvoted_from_last_week
     users = User.all
 
-    # if questions.any?
-    #   users.each do |user|
-    #     WeeklyQuestionMailer.weekly_top_questions(questions, user).deliver_later
-    #   end
-    # else
-    #   users.each do |user|
-    #     WeeklyQuestionMailer.reminder_email(user).deliver_later
-    #   end
-    # end
+    if questions.any?
+      users.each do |user|
+        WeeklyQuestionMailer.weekly_top_questions(questions, user).deliver_later
+      end
+    else
+      users.each do |user|
+        WeeklyQuestionMailer.reminder_email(user).deliver_later
+      end
+    end
   end
 end
