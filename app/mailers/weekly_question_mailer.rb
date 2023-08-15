517a34b687c40c129cb4746610cd912b4eb1f0b4
tyler-wadekamper <wadekampert@spu.edu>
2023-08-14 18:47:54 -0700

    Upload image to hosting server and change mailer.

--- app/mailers/weekly_question_mailer.rb
+++ app/mailers/weekly_question_mailer.rb
@@ -1,6 +1,5 @@
 class WeeklyQuestionMailer < ApplicationMailer
   def weekly_top_questions(questions, user)
-    attachments.inline['white_logo.png'] = File.read('app/assets/images/white_logo.png')
     @questions = questions
     @user = user
     mail(to: @user.email, subject: 'Questions of the Week') do |format|
@@ -9,7 +8,6 @@ class WeeklyQuestionMailer < ApplicationMailer
   end
 
   def reminder_email(user)
-    attachments.inline['white_logo.png'] = File.read('app/assets/images/white_logo.png')
     @user = user
     mail(to: @user.email, subject: 'Have you gotten better today?') do |format|
       format.html
